1afd67e9554bb1e224b685ed91dd5e83bef7dd5e
Danil <chevychelov06122002@gmail.com>
2022-05-20 20:43:24 +0300

    added res to git

---
+++ app/src/test/java/com/example/minesweeper/ExampleUnitTest.kt
@@ -0,0 +1,25 @@
+package com.example.minesweeper
+
+//import org.junit.Test
+import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+//import org.junit.Assert.*
+//import org.junit.Before
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * See [testing documentation](http://d.android.com/tools/testing).
+ */
+class ExampleUnitTest {
+
+    @Test
+    fun addition_isCorrect() {
+        Assertions.assertEquals(4, 2 + 2)
+    }
+    @Test
+    fun addition_isCorrec() {
+        Assertions.assertEquals(4, 2 + 2)
+    }
+}
---
+++ app/src/test/java/com/example/minesweeper/Tests.kt
@@ -0,0 +1,24 @@
+package com.example.minesweeper
+
+import android.os.Looper
+import android.util.Log
+import android.widget.ImageView
+//import org.junit.Test
+//import org.junit.Assert.*
+//import org.junit.Before
+import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+import org.mockito.*
+import org.mockito.BDDMockito.given
+import org.mockito.BDDMockito.mock
+import java.security.SecureRandom
+
+class Tests {
+    @BeforeEach
+    @Test
+    fun relocateBombTest() {
+//        val fieldSizeI = Mockito.spy(GameBoard().fieldSizeI)
+//        GameBoard().attributes()
+    }
+}
